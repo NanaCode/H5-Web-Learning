7764f352cd85d063665d9018e3ac39fd2da55262
NanaCode <2503605326@qq.com>
2018-04-14 21:25:41 +0800

    onLoad and onLeave of H5ComponentBase

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -4,7 +4,7 @@ var H5ComponentBase = function( name, cfg ){
 	var cfg = cfg || {};  //没有参数，空对象
 	var id = ('h5_c_'+Math.random() ).replace('.', '_'); // 为component设置唯一id. 通过id定位component元素
 
-	var cls = 'h5_component_'+cfg.type + ' h5_component_name_'+name;
+	var cls = ' h5_component_name_'+name+ ' h5_component_'+cfg.type;
 	var component = $('<div class="h5_component '+cls+'" id="'+id+'">'); //创建DOM元素
 	// component.appendTo('body');  //可以不这么做
 	cfg.text && component.text(cfg.text);
@@ -20,5 +20,18 @@ var H5ComponentBase = function( name, cfg ){
 			left: '50%'
 		})
 	}
+
+	component.on('onLoad', function(){
+		component.addClass(cls+'_load').removeClass(cls+'_leave');
+		cfg.animateIn && component.animate( cfg.animateIn);
+		return false;
+	})	
+	
+	component.on('onLeave', function(){
+		component.addClass(cls+'_leave').removeClass(cls+'_load'); 
+		cfg.animateOut && component.animate( cfg.animateOut);
+		return false;
+	})
+
 	return component // 直接返回对象
 }
